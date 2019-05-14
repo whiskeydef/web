1b6ea46fd0682269839116fc6b76c66eeb92872a
whiskey def <whiskey_def@163.com>
2019-05-14 15:51:16 +0800

    jq框架demo

---
+++ jq/js/welcome.js
@@ -0,0 +1,25 @@
+function audioAutoPlay(id) {
+    var audio = document.getElementById(id);
+    audio.play();
+    document.addEventListener("WeixinJSBridgeReady", function () {
+        audio.play();
+    }, false);
+}
+
+
+function slide_up(){
+        $("#hi").css("color","#FFAEB9").slideUp(2000);
+}
+function show_text(){
+        $("#hi").show(2000);
+}
+
+$(function() {
+    var audio = $('#media');
+    audio[0].play();
+    $("#audio_btn").bind('click', function() {
+        $(this).hasClass("off") ? ($(this).addClass("play_yinfu").removeClass("off"), $("#yinfu").addClass("rotate"), $("#media")[0].play()) : ($(this).addClass("off").removeClass("play_yinfu"), $("#yinfu").removeClass("rotate"),
+            $("#media")[0].pause());
+    });
+});
+
